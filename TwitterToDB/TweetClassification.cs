d9e2fe81586b0f0f9c97fd4a7d1b05595e5bc2f3
Sanketh Jagadeesh Bhat <SankethJagadeesh_Bhat@intuit.com>
2017-04-09 21:55:04 +0530

    Changed to pull data directly from Twitter service.

---
+++ TwitterToDB/TweetClassification.cs
@@ -0,0 +1,35 @@
+﻿using ml.Attributes;
+using ml.Supervised.NaiveBayes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterToDB
+{
+    internal class TweetClassification
+    {
+        [Label]
+        public string SentimentType { get; set; }
+        [Feature]
+        public int SentimentCount { get; set; }
+        [Feature]
+        public string Language { get; set; }
+        [Feature]
+        public bool IsRetweet { get; set; }
+    }
+
+    internal class TweetClassifer
+    {
+        public static string FindSentimentType(int sentimentCount, bool isRetweet, string language, List<TweetClassification> tweetClassificationList)
+        {
+            var model = new NaiveBayesModel<TweetClassification>();
+            var predictor = model.Generate(tweetClassificationList);
+
+            var result = predictor.Predict(new TweetClassification { SentimentCount = sentimentCount, Language = language, IsRetweet = isRetweet });
+
+            return result.SentimentType;
+        }
+    }
+}
--- TwitterToDB/TwitterToDBClass.cs
+++ TwitterToDB/TwitterToDBClass.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tweetinvi;
-using Tweetinvi.Models;
-using Tweetinvi.Parameters;
-using ml.Supervised.NaiveBayes;
-using ml.Attributes;
+using Newtonsoft.Json;
+using OAuthTwitterWrapper.JsonTypes;
 
 namespace TwitterToDB
 {
@@ -16,65 +10,45 @@ namespace TwitterToDB
     {
         static void Main(string[] args)
         {
-            Auth.SetUserCredentials("Ur3PBOoqVk51myW4EllCbLDq7", "b1vmUfCu3GRoXblMEyjPOuijpECDpvUpsYX8hymFGMhjt5LcHk", "262095897-zJ1P75AIw87di5BjtcLrq3MjlgFwhv0qonHFpDJy", "FQHxPPYwQmnhynZ4fLAIa9IM8S0pDf4gOlZtOT358ndiB");
+            var oAuthTwitterWrapper = new OAuthTwitterWrapper.OAuthTwitterWrapper();
+            var result = JsonConvert.DeserializeObject<Search>(oAuthTwitterWrapper.GetSearch());
 
-            var authenticatedUser = User.GetAuthenticatedUser();
-            var searchParameter = new SearchTweetsParameters("Demonitization")
-            {
-                MaximumNumberOfResults = 500,
-                Until = new DateTime(2017, 06, 02)
-            };
-
-            var tweets = Search.SearchTweets(searchParameter);
-
-            SaveTweets(tweets);
+            SaveTweets(result.Results);
         }
 
-        private static void SaveTweets(IEnumerable<ITweet> tweets)
+        private static void SaveTweets(List<Status> tweets)
         {
             SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Twitter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             connection.Open();
 
             List<TweetClassification> tweetClassifications = new List<TweetClassification>();
 
-            foreach (var tweet in tweets)
+            for (int i = 0; i < 100; i++ )
             {
-                using (SqlCommand com = new SqlCommand("insert into Tweets(TwitterID,FullText,SentimentType,SentimentCount,CreatedDate,Language,IsReweet) values(@TwitterID,@FullText,@SentimentType,@SentimentCount,@CreatedDate,@Language,@IsReweet)", connection))
+                var tweet = tweets.ElementAt(i);
+
+                using (SqlCommand com = new SqlCommand("insert into Tweets(TwitterID,FullText,SentimentType,SentimentCount,CreatedDate,Language,IsReweet,Location) values(@TwitterID,@FullText,@SentimentType,@SentimentCount,@CreatedDate,@Language,@IsReweet,@Location)", connection))
                 {
-                    Sentiment sentiment = SentimentIdentifier.GetSentiment(tweet.FullText);
+                    Sentiment sentiment = SentimentIdentifier.GetSentiment(tweet.Text);
+                    string sentimentType = i <= 90 ? sentiment.Type : TweetClassifer.FindSentimentType(sentiment.Count, tweet.retweeted, tweet.lang, 
+                        tweetClassifications);
 
-                    com.Parameters.AddWithValue("@TwitterID", tweet.Id);
-                    com.Parameters.AddWithValue("@FullText", StopWordRemover.RemoveStopwords(tweet.FullText));
-                    com.Parameters.AddWithValue("@SentimentType", sentiment.Type);
+                    com.Parameters.AddWithValue("@TwitterID", tweet.id.ToString());
+                    com.Parameters.AddWithValue("@FullText", StopWordRemover.RemoveStopwords(tweet.Text));
+                    com.Parameters.AddWithValue("@SentimentType", sentimentType);
                     com.Parameters.AddWithValue("@SentimentCount", sentiment.Count);
-                    com.Parameters.AddWithValue("@CreatedDate", tweet.CreatedAt.ToShortDateString());
-                    com.Parameters.AddWithValue("@Language", tweet.Language.ToString());
-                    com.Parameters.AddWithValue("@IsReweet", tweet.IsRetweet.ToString());
+                    com.Parameters.AddWithValue("@CreatedDate", tweet.CreatedAt);
+                    com.Parameters.AddWithValue("@Language", tweet.lang);
+                    com.Parameters.AddWithValue("@Location", tweet.User.Location);
+                    com.Parameters.AddWithValue("@IsReweet", tweet.retweeted.ToString());
 
-                    tweetClassifications.Add(new TweetClassification { SentimentType = sentiment.Type, SentimentCount = sentiment.Count, IsRetweet = tweet.IsRetweet, Language = tweet.Language.ToString() });
+                    tweetClassifications.Add(new TweetClassification { SentimentType = sentiment.Type, SentimentCount = sentiment.Count, IsRetweet = tweet.retweeted, Language = tweet.lang.ToString() });
 
                     com.ExecuteNonQuery();
                 }
             }
 
-            var model = new NaiveBayesModel<TweetClassification>();
-            var predictor = model.Generate(tweetClassifications.ToArray());
-
-            var result = predictor.Predict(new TweetClassification { SentimentCount = 19, Language = "English", IsRetweet = false });
-
             connection.Close();
-        }
-
-        internal class TweetClassification
-        {
-            [Label]
-            public string SentimentType { get; set; }
-            [Feature]
-            public int SentimentCount { get; set; }
-            [Feature]
-            public string Language { get; set; }
-            [Feature]
-            public bool IsRetweet { get; set; }
         }
     }
 }
